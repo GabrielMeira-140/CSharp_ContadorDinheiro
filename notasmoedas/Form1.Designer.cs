410b74b08c8600f46916dabd6776c966d743b5ec
Gabriel Meira <meiragabriel16@gmail.com>
2020-11-17 18:24:48 -0300

    Contador

---
+++ notasmoedas/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace notasmoedas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numUD1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.richTB1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numUD1
+            // 
+            this.numUD1.DecimalPlaces = 2;
+            this.numUD1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numUD1.Location = new System.Drawing.Point(274, 22);
+            this.numUD1.Maximum = new decimal(new int[] {
+            1215752191,
+            23,
+            0,
+            0});
+            this.numUD1.Name = "numUD1";
+            this.numUD1.Size = new System.Drawing.Size(120, 30);
+            this.numUD1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "digite um numero:";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(400, 22);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(84, 32);
+            this.btnVerificar.TabIndex = 2;
+            this.btnVerificar.Text = "verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // richTB1
+            // 
+            this.richTB1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTB1.Location = new System.Drawing.Point(12, 108);
+            this.richTB1.Name = "richTB1";
+            this.richTB1.Size = new System.Drawing.Size(776, 330);
+            this.richTB1.TabIndex = 3;
+            this.richTB1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTB1);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numUD1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numUD1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numUD1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.RichTextBox richTB1;
+    }
+}
+
---
+++ notasmoedas/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notasmoedas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            richTB1.Clear();
+            double Num = (double)(numUD1.Value);
+            
+            int nota50, nota20, nota10, nota5, nota2, moeda1real, moeda050, moeda025, moeda010, moeda05, moeda001, res;
+
+            res = (int)Num;
+
+            int nota100 = res / 10000;
+
+
+            //parte das notas
+            richTB1.AppendText("NOTAS:" + Environment.NewLine); //notas 
+
+            
+            richTB1.AppendText(nota100 + " nota(s) de R$ 100.00" + Environment.NewLine); //notas de 100
+
+            res = res % 10000;
+            nota50 = res / 5000;
+
+            richTB1.AppendText(nota50 + " nota(s) de R$ 50.00" + Environment.NewLine); //notas de 50
+
+            res = res % 5000;
+            nota20 = res / 2000;
+
+            richTB1.AppendText(nota20 + " nota(s) de R$ 20.00" + Environment.NewLine); //notas de 25
+
+            res = res % 2000;
+            nota10= res / 1000;
+
+            richTB1.AppendText(nota10 + " nota(s) de R$ 10.00" + Environment.NewLine); //notas de 10
+
+            res = res % 1000;
+            nota5 = res / 500;
+
+            richTB1.AppendText(nota5 + " nota(s) de R$ 5.00" + Environment.NewLine); //notas de 5
+
+            res = res % 500;
+            nota2 = res / 200;
+
+            richTB1.AppendText(nota2 + " nota(s) de R$ 2.00" + Environment.NewLine); //notas de 2
+
+            //parte das moedas
+            richTB1.AppendText("MOEDAS:" + Environment.NewLine); // moedas
+
+            res = res % 200;
+            moeda1real = res / 100;
+
+            richTB1.AppendText(moeda1real + " moeda(s) de R$ 1.00" + Environment.NewLine); // moedas de 1 real
+
+            res = res % 100;
+            moeda050 = res / 50;
+
+            richTB1.AppendText(moeda050 + " moeda(s) de R$ 0.50" + Environment.NewLine);  // moedas de 0,50
+
+            res = res % 50;
+            moeda025 = res / 25;
+
+            richTB1.AppendText(moeda025 + " moeda(s) de R$ 0.25" + Environment.NewLine); // moedas de 0,25
+
+            res = res % 25;
+            moeda010 = res / 10;
+
+            richTB1.AppendText(moeda010 + " moeda(s) de R$ 0.10" + Environment.NewLine); // moedas de 0,10
+
+            res = res % 10;
+            moeda05 = res / 5;
+
+            richTB1.AppendText(moeda05 + " moeda(s) de R$ 0.05" + Environment.NewLine); // moedas de 0,05
+
+            res = res % 5;
+            moeda001 = res;
+
+            richTB1.AppendText(moeda001 + " moeda(s) de R$ 0.01" + Environment.NewLine); // moedas de 0,01
+
+
+        }
+    }
+    
+}
